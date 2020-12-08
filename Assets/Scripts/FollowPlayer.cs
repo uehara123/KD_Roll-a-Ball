d1a22866bbd32245d70aa08edc09ef0917017a18
Lunamee <>
2020-12-08 10:37:28 +0900

    FollowPlayer.csの修正
    カメラを一人称視点から三人称視点に変更

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     public Transform target;
+    private Vector3 offset;
+
+    private void Start()
+    {
+        offset = GetComponent<Transform>().position - target.position;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        GetComponent<Transform>().position = target.position;
+        GetComponent<Transform>().position = target.position + offset;
     }
 }

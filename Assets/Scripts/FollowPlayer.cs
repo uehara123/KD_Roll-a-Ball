fcfaf73f89d347ecdb189feca0a3ecb16f747111
Lunamee <>
2020-12-08 10:27:07 +0900

    PlayerCameraを実装
    Playerを追従するカメラを実装

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Transform target;
 
     // Update is called once per frame
     void Update()
     {
-        
+        GetComponent<Transform>().position = target.position;
     }
 }
